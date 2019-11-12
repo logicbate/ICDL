72a69d55e7c447e9f9d52c59fa8841a4c18eb8cf
Hermann KOKO <batehermann@gmail.com>
2019-11-12 11:20:51 +0000

    first commit

---
+++ emails/verification.blade.php
@@ -0,0 +1,300 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
+<head>
+<!--[if (gte mso 9)|(IE)]><xml><o:OfficeDocumentSettings><o:AllowPNG/><o:PixelsPerInch>96</o:PixelsPerInch></o:OfficeDocumentSettings></xml><![endif]-->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1"> <!-- So that mobile will display zoomed in -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge"> <!-- enable media queries for windows phone 8 -->
+<meta name="format-detection" content="telephone=no"> <!-- disable auto telephone linking in iOS -->
+<meta name="format-detection" content="date=no"> <!-- disable auto date linking in iOS -->
+<meta name="format-detection" content="address=no"> <!-- disable auto address linking in iOS -->
+<meta name="format-detection" content="email=no"> <!-- disable auto email linking in iOS -->
+<title></title>
+
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,700,700i" rel="stylesheet">
+
+<!--[if (gte mso 9)|(IE)]>
+<style type="text/css">
+  table, tr, td, div, span, p, a {font-family: Arial,Helvetica,sans-serif !important;}
+  td a {color: inherit !important; text-decoration: none !important;}
+  a {color: inherit !important; text-decoration: none !important;}
+</style>
+<![endif]-->
+
+<style type="text/css">
+/*Basics*/
+body {margin:0px !important; padding:0px !important; display:block !important; min-width:100% !important; width:100% !important; -webkit-text-size-adjust:none;}
+table {border-spacing:0; mso-table-lspace:0pt; mso-table-rspace:0pt;}
+table td {border-collapse: collapse;}
+strong {font-weight: bold !important;}
+img {-ms-interpolation-mode:bicubic; display:block; width:auto; max-width:auto; height:auto; margin:auto;}
+p {margin:0 !important; padding:0 !important; display:inline-block !important; font-family:inherit !important;}
+a {text-decoration:none !important;}
+/*Outlook*/
+.ExternalClass {width: 100%;}
+.ExternalClass,.ExternalClass p,.ExternalClass span,.ExternalClass font,.ExternalClass td,.ExternalClass div {line-height:inherit;}
+.ReadMsgBody {width:100%; background-color: #ffffff;}
+/* iOS BLUE LINKS */
+a[x-apple-data-detectors] {color:inherit !important; text-decoration:none !important; font-size:inherit !important; font-family:inherit !important; font-weight:inherit !important; line-height:inherit !important;}
+/*Gmail blue links*/
+u + #body a {color:inherit;text-decoration:none;font-size:inherit;font-family:inherit;font-weight:inherit;line-height:inherit;}
+/*MailChimp preview text*/
+.mcnPreviewText{display: none !important;}
+/*Buttons fix*/
+.undoreset a, .undoreset a:hover {text-decoration:none !important;}
+.yshortcuts a {border-bottom:none !important;}
+.ios-footer a {color:#aaaaaa !important;text-decoration:none;}
+/*Responsive*/
+@media screen and (max-width: 599px) {
+  td.img-responsive img {width:100%!important;max-width: 100%!important;height: auto!important;margin: auto;}
+  table.row {width: 100%!important;max-width: 100%!important;}
+  table.center-float, td.center-float {float: none!important;}
+  td.center-text{text-align: center!important;}
+  td.container-padding {width: 100%!important;padding-left: 20px!important;padding-right: 20px!important;}
+  table.hide-mobile, tr.hide-mobile, td.hide-mobile, br.hide-mobile {display: none!important;}
+  td.menu-container {text-align: center !important;}
+  td.autoheight {height: auto!important;}
+  td.height200 {height: 200px!important;}
+  td.border-rounded {border-radius: 6px!important;}
+  td.border-none {border: none!important;}
+  table.mobile-padding {margin: 15px 0!important;}
+}
+</style>
+
+</head>
+
+<body id="body" style="margin-top: 0; margin-bottom: 0; padding-top: 0; padding-bottom: 0; width: 100%; -webkit-text-size-adjust: 100%; -ms-text-size-adjust: 100%;" bgcolor="#F4F4F4">
+
+<!-- hm-3 -->
+
+<table border="0" width="100%" align="center" cellpadding="0" cellspacing="0"       style="width:100%;max-width:100%;">
+  <tr>
+    <td class="sion-hm-3" align="center" valign="top">
+
+<!-- body-bg-color -->
+<table border="0" width="100%" align="center" cellpadding="0" cellspacing="0" class="row" style="width:100%;max-width:100%;">
+  <tr>
+    <td class="body-bg-color" align="center" valign="top"    bgcolor="#F4F4F4">
+
+<!-- bg-color -->
+<table border="0" width="800" align="center" cellpadding="0" cellspacing="0" class="row" style="width:800px;max-width:800px;">
+  <tr>
+    <td class="bg-color" align="center" valign="top"    bgcolor="#f2564a">
+
+<!--[if (gte mso 9)|(IE)]><table border="0" cellpadding="0" cellspacing="0"><tr><td valign="top"><![endif]-->
+
+<!-- column -->
+<table width="400" border="0" cellpadding="0" cellspacing="0" align="left" class="row" style="width:400px;max-width:400px;">
+  <tr>
+    <td align="center" valign="middle">
+
+<!--[if (gte mso 9)|(IE)]>
+<v:rect xmlns:v="urn:schemas-microsoft-com:vml" fill="true" stroke="false" style="width:400px;height:515px;">
+<v:fill type="frame" src="https://www.psd2newsletters.com/templates/sion/img/hm-pict-3.jpg" color="#f2564a"/>
+<v:textbox style="v-text-anchor:middle;" inset="0,0,0,0">
+<![endif]-->
+
+<!-- hm-3-bg-image -->
+<table border="0" width="100%" align="center" cellpadding="0" cellspacing="0" class="row" style="width:100%;max-width:100%;">
+  <tr>
+    <td class="bg-image autoheight" height="515" align="center" valign="top" background="img/photo-item5.png"  style="background-size:cover; background-position: center top;">
+
+<!--[if (gte mso 9)|(IE)]><table border="0" cellpadding="0" cellspacing="0"><tr><td valign="top" width="100"></td><td valign="top"><![endif]-->
+
+<!-- column -->
+<table width="300" border="0" cellpadding="0" cellspacing="0" align="right" class="row" style="width:300px;max-width:300px;">
+  <tr>
+    <td align="center" valign="top">
+<!-- space -->
+<table border="0" width="100%" align="center" cellpadding="0" cellspacing="0" style="width:100%;max-width:100%;">
+  <tr>
+    <td valign="middle" align="center" height="50" class="height200">  &nbsp;  </td>
+  </tr>
+</table>
+<!-- space -->
+<!-- logo -->
+<table width="100%" border="0" cellpadding="0" cellspacing="0" align="center" style="width:100%;max-width:100%;">
+  <tr>
+    <td align="center" valign="middle">
+      <table border="0" cellpadding="0" cellspacing="0" align="left" class="center-float">
+        <tr>
+          <td class="logo" valign="middle" align="left" height="20">
+            <img style="display:block;" src="/img/logo-colored.png"  border="0"  alt="logo">
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+<!-- logo -->
+<!-- space -->
+<table border="0" width="100%" align="center" cellpadding="0" cellspacing="0" style="width:100%;max-width:100%;">
+  <tr>
+    <td valign="middle" align="center" height="50" class="height200">  &nbsp;  </td>
+  </tr>
+</table>
+<!-- space -->
+    </td>
+  </tr>
+</table>
+<!-- column -->
+
+<!--[if (gte mso 9)|(IE)]></td></tr></table><![endif]-->
+
+    </td>
+  </tr>
+</table>
+<!-- hm-3-bg-image -->
+
+<!--[if (gte mso 9)|(IE)]></v:textbox></v:rect><![endif]-->
+
+    </td>
+  </tr>
+</table>
+<!-- column -->
+
+<!--[if (gte mso 9)|(IE)]></td><td valign="top"><![endif]-->
+
+<!-- column -->
+<table width="400" border="0" align="left" cellpadding="0" cellspacing="0" class="row" style="width:400px;max-width:400px;">
+  <tr>
+    <td class="bg-color autoheight" align="center" valign="top"    height="515" bgcolor="#f2564a">
+
+<!-- container -->
+<table border="0" width="280" align="center" cellpadding="0" cellspacing="0" style="width:280px;max-width:280px;">
+  <tr>
+    <td align="center" valign="top" class="container-padding">
+
+<!-- space -->
+<table border="0" width="100%" align="center" cellpadding="0" cellspacing="0" style="width:100%;max-width:100%;">
+  <tr>
+    <td valign="middle" align="center" height="50">  &nbsp;  </td>
+  </tr>
+</table>
+<!-- space -->
+
+
+
+<!-- space -->
+<table border="0" width="100%" align="center" cellpadding="0" cellspacing="0" style="width:100%;max-width:100%;">
+  <tr>
+    <td valign="middle" align="center" height="40">  &nbsp;  </td>
+  </tr>
+</table>
+<!-- space -->
+
+<!-- title -->
+<table   border="0" width="100%" cellpadding="0" cellspacing="0" align="center" style="width:100%; max-width:100%;">
+  <tr>
+    <td class="title center-text"      valign="middle" align="left" style="font-family:'Poppins',Arial,Helvetica,sans-serif; font-size:35px; line-height:45px; font-weight:lighter;font-style:normal; color:#FFFFFF;text-decoration:none;letter-spacing: 0px;">
+
+        Plus qu'une derniére étape pour être connecté au monde entier !
+
+    </td>
+  </tr>
+</table>
+<!-- title -->
+
+<!-- space -->
+<table border="0" width="100%" align="center" cellpadding="0" cellspacing="0" style="width:100%;max-width:100%;">
+  <tr>
+    <td valign="middle" align="center" height="20">  &nbsp;  </td>
+  </tr>
+</table>
+<!-- space -->
+
+
+
+<!-- 2 buttons in row -->
+<table border="0" width="100%" cellpadding="0" cellspacing="0" align="center" style="width:100%; max-width:100%;">
+  <tr>
+    <td valign="middle" align="center" class="buttons-row">
+
+<!-- Buttons-Container -->
+<table border="0" cellpadding="0" cellspacing="0" align="left" class="center-float">
+  <tr>
+    <td align="center" valign="middle">
+
+<!--[if (gte mso 9)|(IE)]><table border="0" cellpadding="0" cellspacing="0"><tr><td valign="middle"><![endif]-->
+
+<!-- Button-Bg -->
+<table   border="0" cellpadding="0" cellspacing="0" align="left" class="center-float" style="display:inline-block;vertical-align:middle;">
+  <tr>
+    <td align="center" valign="top">
+<!-- Container -->
+<table border="0" cellpadding="0" cellspacing="0" align="center" class="center-float" style="display:inline-block;vertical-align:middle;">
+  <tr>
+    <td class="button-bg" valign="middle" align="center"    bgcolor="#FFFFFF" style="border-radius: 6px;">
+    <!-- Btn -->
+    <table border="0" cellpadding="0" cellspacing="0" align="center" class="center-float">
+      <tr>
+        <td colspan="3" height="15" style="font-size:15px;line-height:15px;">&nbsp;</td>
+      </tr>
+      <tr>
+        <td width="20"></td>
+        <td valign="middle" align="center"      style="font-family:'Poppins',Arial,Helvetica,sans-serif;font-size:14px;line-height:14px;font-weight:normal;font-style:normal;color:#f2564a;text-decoration:none;letter-spacing:0px;">
+
+              <a href="{{ url('register/verify/'.$user->email_token) }}" target="_blank" style="color: #f2564a;">Confirmer mon adresse</a>
+
+        </td>
+        <td width="20"></td>
+      </tr>
+      <tr>
+        <td colspan="3" height="15" style="font-size:15px;line-height:15px;">&nbsp;</td>
+      </tr>
+    </table>
+    <!-- Btn -->
+    </td>
+  </tr>
+</table>
+<!-- Container -->
+    </td>
+  </tr>
+</table>
+<!-- Button-Bg -->
+
+    </td>
+  </tr>
+</table>
+<!-- Buttons-Container -->
+
+    </td>
+  </tr>
+</table>
+<!-- 2 buttons in row -->
+
+
+    </td>
+  </tr>
+</table>
+<!-- container -->
+
+    </td>
+  </tr>
+</table>
+<!-- column -->
+
+<!--[if (gte mso 9)|(IE)]></td></tr></table><![endif]-->
+
+    </td>
+  </tr>
+</table>
+<!-- bg-color -->
+
+    </td>
+  </tr>
+</table>
+<!-- body-bg-color -->
+
+    </td>
+  </tr>
+</table>
+
+
+    </td>
+  </tr>
+</table>
+
+
+</body>
+</html>
